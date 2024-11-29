d2c62ec217696ab6337e47b7ff5f86123e39d6db
Karen Guzman <krngzmn10@gmail.com>
2024-11-29 00:08:57 -0500

    back to original github provided code

--- pacman/app.py
+++ pacman/app.py
@@ -17,69 +17,65 @@ s3 = boto3.client('s3')
 ## UPDATE NEXT LINE
 baseurl = 'http://sae3gg-dp1-spotify.s3-website-us-east-1.amazonaws.com/'
 
-# database things
+ # database things
 DBHOST = os.getenv('DBHOST')
-
 DBUSER = os.getenv('DBUSER')
 DBPASS = os.getenv('DBPASS')
 DB = os.getenv('DB')
-#db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-#cur = db.cursor()
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur = db.cursor()
 
 # file extensions to trigger on
 _SUPPORTED_EXTENSIONS = (
-    '.json'
+  '.json'
 )
-
-# Function to create a fresh database connection
-def create_db_connection():
-    return mysql.connector.connect(
-        user=DBUSER,
-        host=DBHOST,
-        password=DBPASS,
-        database=DB,
-        connection_timeout=300  # Ensure the connection does not timeout
-    )
 
 # ingestor lambda function
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'])
 def s3_handler(event):
-    if _is_json(event.key):
-        # Get the file, read it, and load it into JSON as an object
-        response = s3.get_object(Bucket=S3_BUCKET, Key=event.key)
-        text = response["Body"].read().decode()
-        data = json.loads(text)
+  if _is_json(event.key):
+    # get the file, read it, load it into JSON as an object
+    response = s3.get_object(Bucket=S3_BUCKET, Key=event.key)
+    text = response["Body"].read().decode()
+    data = json.loads(text)
 
-        TITLE = data.get('title', 'Unknown Title')
-        ALBUM = data.get('album', 'Unknown Album')
-        ARTIST = data.get('artist', 'Unknown Artist')
-        YEAR = data.get('year', 0)
-        GENRE = data.get('genre', 'Unknown Genre')
+    # parse the data fields 1-by-1 from 'data'
+    TITLE = data.get('title', 'Unknown Title')
+    ALBUM = data.get('album', 'Unknown Album')
+    ARTIST = data.get('artist', 'Unknown Artist')
+    YEAR = data.get('year', 0)
+    GENRE = data.get('genre', 'Unknown Genre')
 
-        identifier = event.key.split('.')[0]
-        MP3 = f"{baseurl}{identifier}.mp3"
-        IMG = f"{baseurl}{identifier}.jpg"
+    # get the unique ID for the bundle to build the mp3 and jpg urls
+    # you get 5 data points in each new JSON file that arrives, but
+    # you need 7 fields for the INSERT. The two additional values are
+    # URLs you must formulate given you know the unique ID these files
+    # are named.
+    keyhead = event.key
+    identifier = keyhead.split('.')
+    ID = identifier[0]
+    MP3 = baseurl + ID + '.mp3'
+    IMG = baseurl + ID + '.jpg'
 
-        app.log.debug("Received new song: %s, key: %s", event.bucket, event.key)
+    app.log.debug("Received new song: %s, key: %s", event.bucket, event.key)
 
-        try:
-            # Use a fresh connection and cursor for each Lambda invocation
-            with create_db_connection() as db:
-                # Using the connection's cursor in a with block to ensure proper closing
-                with db.cursor() as cur:
-                    add_song = (
-                        "INSERT INTO songs "
-                        "(title, album, artist, year, file, image, genre) "
-                        "VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                    )
-                    song_vals = (TITLE, ALBUM, ARTIST, YEAR, MP3, IMG, GENRE)
-                    cur.execute(add_song, song_vals)
-                    db.commit()
-                    app.log.debug("Song inserted successfully into the database")
+    # try to insert the song into the database
+    try:
+      add_song = ("INSERT INTO songs "
+               "(title, album, artist, year, file, image, genre) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+      song_vals = (TITLE, ALBUM, ARTIST, YEAR, MP3, IMG, GENRE)
+      cur.execute(add_song, song_vals)
+      db.commit()
+      cur.close()
+      db.close()
 
-        except mysql.connector.Error as err:
-            app.log.error("Failed to insert song: %s", err)
+    except mysql.connector.Error as err:
+      app.log.error("Failed to insert song: %s", err)
+      db.rollback()
+      cur.close()
+      db.close()
 
-# Perform a suffix match against supported extensions
+# perform a suffix match against supported extensions
 def _is_json(key):
-    return key.endswith(_SUPPORTED_EXTENSIONS)
+  return key.endswith(_SUPPORTED_EXTENSIONS)
